5eb02385213ee469c4451b74df0e17e4f83b457e
Miguel Ángel Barrera <mbarrera@plainconcepts.com>
2021-10-26 21:37:09 +0200

    first commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+
+namespace DarkLightMode
+{
+	public partial class MainPage : ContentPage
+	{
+		int count = 0;
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private void OnCounterClicked(object sender, EventArgs e)
+		{
+			count++;
+			CounterLabel.Text = $"Current count: {count}";
+
+			SemanticScreenReader.Announce(CounterLabel.Text);
+		}
+
+		void LightModeClicked(object sender, EventArgs eventArgs)
+        {
+			MainThread.InvokeOnMainThreadAsync(() =>
+			{
+				App.Current.UserAppTheme = OSAppTheme.Light;
+
+			});
+        }
+		
+		void DarkModeClicked(object sender, EventArgs eventArgs)
+        {
+			MainThread.InvokeOnMainThreadAsync(() =>
+			{
+				App.Current.UserAppTheme = OSAppTheme.Dark;
+
+			});
+		}
+	}
+}
